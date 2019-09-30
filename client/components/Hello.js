a51c108b6a0623943c17942331bdb47128a83101
Dominic Torruellas <dom@domtorr.com>
2019-09-30 16:11:36 -0700

    tailwind setup and config files

---
+++ client/components/Hello.js
@@ -0,0 +1,3 @@
+const Hello = () => <div className="text-blue-600 text-2xl">Hello World</div>;
+
+export default Hello;
---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import '../styles/index.css';
+
+import Hello from '../components/Hello';
+
+const Home = () => (
+  <div>
+    <Hello></Hello>
+  </div>
+);
+
+export default Home;
