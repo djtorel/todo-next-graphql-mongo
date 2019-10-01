df98af4dd9e498f0389ac7432844647ddc79dbba
Dominic Torruellas <dom@domtorr.com>
2019-09-30 17:55:45 -0700

    initial server config with babel and apollo

---
+++ server/src/index.js
@@ -0,0 +1,29 @@
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { typeDefs } from './typeDefs';
+import { resolvers } from './resolvers';
+
+const app = express();
+
+const startServer = async () => {
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  server.applyMiddleware({ app });
+
+  await mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // Launch the server
+  const port = process.env.PORT || 4000;
+  app.listen(port, () =>
+    console.log(
+      `🚀  Server ready at http://localhost:${port}${server.graphqlPath}`
+    )
+  );
+};
+
+startServer();
